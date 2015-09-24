608123d073aa3effeb722571e98bf566d5676213
Marcin Zaremba <marcin.zaremba@gmail.com>
2015-09-24 14:17:31 +0200

    Add script to generate new virtual host for nginx

---
+++ bash/AddLocalHostNginx/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+user="marcinos"
+user_mail="marcin.zaremba@gmail.com"
+nginxSitesAvailablePath="/etc/nginx/sites-available/"
+nginxSitesEnabledPath="/etc/nginx/sites-enabled/"
+hostsPath="/etc/hosts"
+htdocs="/var/www/html/"
